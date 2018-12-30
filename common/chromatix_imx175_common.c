287ac641eb3a0fb714ff568135a11cf88337be1a
PythonLimited <Leonardomeitz@gmail.com>
2018-12-30 16:50:08 +0100

    imx175 bringup

---
+++ common/chromatix_imx175_common.c
@@ -0,0 +1,27 @@
+/*============================================================================
+
+  Copyright (c) 2012-2013 Qualcomm Technologies, Inc. All Rights Reserved.
+  Qualcomm Technologies Proprietary and Confidential.
+
+============================================================================*/
+
+/*============================================================================
+ *                      INCLUDE FILES
+ *===========================================================================*/
+#include "chromatix_common.h"
+#include "sensor_dbg.h"
+
+static chromatix_VFE_common_type chromatix_imx175_parms = {
+#include "chromatix_imx175_common.h"
+};
+
+/*============================================================================
+ * FUNCTION    - load_chromatix -
+ *
+ * DESCRIPTION:
+ *==========================================================================*/
+void *load_chromatix(void)
+{
+  SLOW("chromatix ptr %p", &chromatix_imx175_parms);
+  return &chromatix_imx175_parms;
+}
